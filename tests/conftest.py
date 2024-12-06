c3b6dd40195af878e9fe38cbc74385e6c2be77f7
Sergey B Kirpichev <skirpichev@gmail.com>
2024-12-06 09:58:22 +0300

    Relax default deadline even more

--- tests/conftest.py
+++ tests/conftest.py
@@ -6,6 +6,6 @@ from hypothesis import settings
 default = settings.get_profile("default")
 settings.register_profile("default",
                           settings(default,
-                                   deadline=timedelta(milliseconds=300)))
+                                   deadline=timedelta(seconds=300)))
 ci = settings.get_profile("ci")
 settings.register_profile("ci", settings(ci, max_examples=10000))
