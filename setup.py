e7ea8f3a50ca8f24e30080b53f12bb5b396b8b55
Sergey B Kirpichev <skirpichev@gmail.com>
2024-12-31 06:56:02 +0300

    Don't use new ext-modules directive

--- setup.py
+++ setup.py
@@ -1,5 +1,20 @@
 # FIXME: see https://github.com/pypa/setuptools/issues/3025
-from setuptools import setup
+import os
+import platform
+
+from setuptools import Extension, setup
 
 
-setup()
+ON_WINDOWS = platform.system() == "Windows"
+if os.getenv("CIBUILDWHEEL"):
+    include_dirs = [os.path.join(os.path.dirname(__file__), ".local",
+                                 "include")]
+    library_dirs = [os.path.join(os.path.dirname(__file__), ".local",
+                                 "bin" if ON_WINDOWS else "lib")]
+else:
+    include_dirs = []
+    library_dirs = []
+
+setup(ext_modules=[Extension("gmp", sources=["main.c"], libraries=["gmp"],
+                             include_dirs=include_dirs,
+                             library_dirs=library_dirs)])
