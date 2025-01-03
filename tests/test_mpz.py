6c47bd9925fd626b62377b0e260c2ec38b7e3b7a
Sergey B Kirpichev <skirpichev@gmail.com>
2025-01-03 14:00:36 +0300

    Link to PyPy issue

--- tests/test_mpz.py
+++ tests/test_mpz.py
@@ -606,8 +606,8 @@ def test_from_bytes(x, length, byteorder, signed):
         rx = int.from_bytes(bytes, byteorder, signed=signed)
         assert rx == mpz.from_bytes(bytes, byteorder, signed=signed)
         assert rx == mpz.from_bytes(bytearray(bytes), byteorder, signed=signed)
-        if platform.python_implementation() == "PyPy":  # FIXME
-            return
+        if platform.python_implementation() == "PyPy":
+            return  # XXX: pypy/pypy#5165
         assert rx == mpz.from_bytes(list(bytes), byteorder, signed=signed)
 
 
