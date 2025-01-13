b99c39085e17456e156dbde8369706641b539b39
Sergey B Kirpichev <skirpichev@gmail.com>
2025-01-13 08:27:39 +0300

    - debug

--- tests/test_functions.py
+++ tests/test_functions.py
@@ -31,7 +31,6 @@ def test_factorial(x):
 def test_factorial_outofmemory(x):
     soft, hard = resource.getrlimit(resource.RLIMIT_AS)
     resource.setrlimit(resource.RLIMIT_AS, (1024*64*1024, hard))
-    print(x)
     x = mpz(x)
     while True:
         try:
