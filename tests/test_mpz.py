f5a6cebeea075dbb77bcbf57b5f95044f7a3368e
Sergey B Kirpichev <skirpichev@gmail.com>
2025-07-12 12:55:22 +0300

    Filter test_mpz_interface() for GraalVM

--- tests/test_mpz.py
+++ tests/test_mpz.py
@@ -148,6 +148,8 @@ def test_from_floats(x):
     assert mpz(x) == int(x)
 
 
+@pytest.mark.skipif(platform.python_implementation() == "GraalVM",
+                    reason="XXX")
 def test_mpz_interface():
     with pytest.raises(ValueError):
         mpz(123).digits(-1)
